e00d26875e905d7e5feeb5724abf07f7938dd14b
wlehgml94 <wlehgml94@naver.com>
2023-11-10 14:23:19 +0900

    231110 :: 삭제버튼 추가

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
 import ReviewList from "./ReviewList";
-import items from '../mock.json';
+import mockItems from '../mock.json';
 import { useState } from "react";
 
 function App(){
+    const [items, setItems] = useState(mockItems);
     const [order, setOrder] = useState('createdAt');
     const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
     const handleNewestClick = () => setOrder('createdAt');
     const handleBestClick = () => setOrder('rating');
+
+    const handleDelete = (id) => {
+        const nextItems = items.filter((item) => item.id !== id);
+        setItems(nextItems);
+    }
 
     return (
         <div>
@@ -15,7 +21,7 @@ function App(){
                 <button onClick={handleNewestClick}>최신순</button>
                 <button onClick={handleBestClick}>베스트순</button>
             </div>
-            <ReviewList items={sortedItems} />
+            <ReviewList items={sortedItems} onDelete={handleDelete} />
         </div>
     );
 }
--- src/components/ReviewList.js
+++ src/components/ReviewList.js
@@ -4,7 +4,8 @@ function formatData(value){
     const date = new Date(value);
     return `${date.getFullYear()}. ${date.getMonth() + 1}. ${date.getDate()}`
 }
-function ReviewListItem({item}){
+function ReviewListItem({item, onDelete}){
+    const handleDeleteClick = () => onDelete(item.id);
     return(
         <div className="ReviewListItem">
             <img className="ReviewListItem-img" src={item.imgUrl} alt={item.title} />
@@ -13,15 +14,16 @@ function ReviewListItem({item}){
                 <p>{item.rating}</p>
                 <p>{formatData(item.createdAt)}</p>
                 <p>{item.content}</p>
+                <button onClick={handleDeleteClick}>삭제</button>
             </div>
         </div>
     );
 };
-function ReviewList({items}) {
+function ReviewList({items, onDelete}) {
     return (
         <ul>
             {items.map((item) =>{
-                return <li><ReviewListItem item={item} /></li>;
+                return <li><ReviewListItem item={item} onDelete={onDelete} /></li>;
             })}
         </ul>
     );
