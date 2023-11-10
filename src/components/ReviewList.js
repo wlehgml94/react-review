4f9b1c716b55375cac885b290405116a529b3398
wlehgml94 <wlehgml94@naver.com>
2023-11-10 14:13:09 +0900

    git start

---
+++ src/components/ReviewList.js
@@ -0,0 +1,29 @@
+import './ReviewList.css';
+
+function formatData(value){
+    const date = new Date(value);
+    return `${date.getFullYear()}. ${date.getMonth() + 1}. ${date.getDate()}`
+}
+function ReviewListItem({item}){
+    return(
+        <div className="ReviewListItem">
+            <img className="ReviewListItem-img" src={item.imgUrl} alt={item.title} />
+            <div>
+                <h1>{item.title}</h1>
+                <p>{item.rating}</p>
+                <p>{formatData(item.createdAt)}</p>
+                <p>{item.content}</p>
+            </div>
+        </div>
+    );
+};
+function ReviewList({items}) {
+    return (
+        <ul>
+            {items.map((item) =>{
+                return <li><ReviewListItem item={item} /></li>;
+            })}
+        </ul>
+    );
+}
+export default ReviewList;
